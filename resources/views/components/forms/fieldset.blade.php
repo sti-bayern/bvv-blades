38e6131ef83c3e0671cab8c93417ace9f50f4c59
Krompaß, Matthias (ADBV A) <Matthias.Krompass@adbv-a.bayern.de>
2024-07-12 09:39:03 +0200

    Bugfix: Legend bei Fieldset

--- resources/views/components/forms/fieldset.blade.php
+++ resources/views/components/forms/fieldset.blade.php
@@ -6,6 +6,7 @@
     @if ($legend)
         <legend
             class="rounded-t-sm block bg-gray-100 border-x border-t px-2 py-2 font-bold border-gray-200 w-full -py-6 ">
+            {{ $legend }}
         </legend>
     @endif
     <div
