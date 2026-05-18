5e93966eeec077f71b4113d0657e97f8b0856a21
Dennis Kubousek <Dennis.Kubousek@adbv-a.bayern.de>
2026-05-18 10:55:12 +0000

    Kontrastanpassung des Burger-Menüs

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -59,7 +59,7 @@
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
             <div class="flex items-center md:order-2 p-4">
                 <button data-collapse-toggle="mega-menu" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-100 hover:text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="mega-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
